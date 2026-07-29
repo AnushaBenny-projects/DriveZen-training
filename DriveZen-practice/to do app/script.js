98513ceb3736b7c85652598e31172deb10b7e0f3
AnushaBenny-projects <anushabenny12345@gmail.com>
2026-07-29 08:12:20 +0530

    Implement local storage for todo tasks
    Added functionality to store tasks in local storage.

--- DriveZen-practice/to do app/script.js
+++ DriveZen-practice/to do app/script.js
@@ -5,33 +5,34 @@ const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 
-// Form submit event
+let todos = [];
+
 
 form.addEventListener("submit", function(event) {
 
-    // Stop page refresh
+    
     event.preventDefault();
 
-    // Get input value
+  
     const task = taskInput.value.trim();
 
-    // Check input is not empty
     if (task !== "") {
+        
+        todos.push(task);
+        localStorage.setItem("todos", JSON.stringify(todos));
 
-        // Create new li
         const li = document.createElement("li");
 
-        // Add task text
+     
         li.innerText = task;
 
 
-        // Create Delete button
         const deleteButton = document.createElement("button");
 
         deleteButton.innerText = "Delete";
 
 
-        // Delete button event
+    
         deleteButton.addEventListener("click", function() {
 
             li.remove();
@@ -39,15 +40,14 @@ form.addEventListener("submit", function(event) {
         });
 
 
-        // Add delete button to li
+
         li.appendChild(deleteButton);
 
 
-        // Add li to ul
+  
         taskList.appendChild(li);
 
 
-        // Clear input
         taskInput.value = "";
     }
 
